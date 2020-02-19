0520406c57208f12d6d1f100a1a9d4bbf7697200
rudenoise <rudenoise@gmail.com>
2020-02-19 16:06:41 +0000

    create a flow from poc app to idm mock and back

--- poc_front_end/src/server.js
+++ poc_front_end/src/server.js
@@ -12,7 +12,22 @@ server.route({
   method: 'GET',
   path: '/',
   handler: (request, h) => {
-    return `<p id='hello-world'>Hello World from ${process.env.HOST_NAME}!<br>${JSON.stringify(process.env)}</p><br>${JSON.stringify(server.info)}`
+    return `<H1 id="hello-world">Hello World from ${process.env.HOST_NAME}!</H1>
+    <p>Fake a sign in via IDM <a href="${process.env.IDM_HOST}?redirect_path=/redirect_path">here...</a></p>
+    `
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/redirect_path',
+  handler: (request, h) => {
+    return `
+      <h1>You just came back from IDM (mock)</h1>
+      <pre>
+${JSON.stringify(request.query, ' ', 2)}
+      </pre>
+    `
   }
 })
 
