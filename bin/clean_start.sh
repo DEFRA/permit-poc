8ca6b2721ae876aea34e74b0cebdcad9acd1281c
rudenoise <rudenoise@gmail.com>
2020-02-20 11:23:46 +0000

    tidy up idm container and add makefile

--- bin/clean_start.sh
+++ bin/clean_start.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 # build, ensuring all layers are removed first, "nuclear" option
 # that removes all the efficiancies of caching layers
 docker build --no-cache -t poc_front_end poc_front_end/
+docker build --no-cache -t poc_idm_mock idm_mock/
 
 # copy the node files so that they can be added to version control
 docker run -i poc_front_end /bin/cat /home/node/package.json > ./poc_front_end/package.json
