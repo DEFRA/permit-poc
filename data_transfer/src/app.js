7bd236b272b1543bc11f7ed1121554e81f20fbfd
rudenoise <rudenoise@gmail.com>
2020-02-21 16:27:25 +0000

    WE-3540: sketch out sending data to azure blob storage

--- data_transfer/src/app.js
+++ data_transfer/src/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const { BlobServiceClient } = require('@azure/storage-blob')
+const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob")
+const {promisify} = require('util')
 
 async function main() {
     console.log('Azure Blob storage v12 - JavaScript quickstart sample');
@@ -8,8 +9,31 @@ async function main() {
 }
 
 main()
-  .then(() => {
-    console.log('Done!!!!')
+  .then(async () => {
+    console.log('BLOB_CONTAINER_NAME:', process.env.BLOB_CONTAINER_NAME)
+    console.log('AZURE_STORAGE_CONNECTION_STRING:', process.env.AZURE_STORAGE_CONNECTION_STRING)
+    console.log()
+
+    const blobServiceClient = await BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING)
+
+    console.log(blobServiceClient)
+    console.log('=========')
+    const containerName = process.env.BLOB_CONTAINER_NAME
+    const containerClient = blobServiceClient.getContainerClient(containerName)
+    const content = "Hello world!";
+    const blobName = "newblob" + new Date().getTime();
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    console.log(blockBlobClient)
+    console.log('=========')
+    return blockBlobClient.upload(content, content.length)
+
+  })
+  .then(uploadBlobResponse => {
+    console.log('*******')
+    console.log(`Upload block blob successfully`, uploadBlobResponse.requestId);
+    //for await (const blob of iter) {
+    //  console.log(`Blob ${i++}: ${blob.name}`)
+    //}
     process.exit(0)
   })
   .catch((ex) => {
