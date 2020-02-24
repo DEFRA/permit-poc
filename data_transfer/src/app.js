82b7443b03ef14d16f31e7770bf99aa56f188bf9
rudenoise <rudenoise@gmail.com>
2020-02-24 15:28:53 +0000

    WE-3540: basic refactor

--- data_transfer/src/app.js
+++ data_transfer/src/app.js
@@ -19,55 +19,29 @@ async function sendToDynamics(obj, label) {
 
 }
 
+function validateMessage() {
+  const ajv = new Ajv()
+  const validate = ajv.compile(schema)
+  return validate(message)
+}
+
 async function main() {
-    console.log('Azure Blob storage v12 - JavaScript quickstart sample');
-    // Quick start code goes here
+    console.log('ATTEMPT TO VALIDATE USING JSON_SCHEMA AND SEND TO DYNAMICS')
+    try {
+      if (validateMessage()) {
+        console.log('SUCESS, MESSAGE VALID')
+        await sendToDynamics(schema, 'schema')
+        await sendToDynamics(message, 'message')
+        console.log('SUCCESS, SCHEMA AND MESSAGE SENT')
+        process.exit(0)
+      } else {
+        console.error('Invalid message')
+        process.exit(1)
+      }
+    } catch(e) {
+      console.error(e)
+      process.exit(1)
+    }
 }
 
 main()
-  /*
-  .then(async () => {
-    console.log('BLOB_CONTAINER_NAME:', process.env.BLOB_CONTAINER_NAME)
-    console.log('AZURE_STORAGE_CONNECTION_STRING:', process.env.AZURE_STORAGE_CONNECTION_STRING)
-    console.log()
-
-    const blobServiceClient = await BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING)
-
-    console.log(blobServiceClient)
-    console.log('=========')
-    const containerName = process.env.BLOB_CONTAINER_NAME
-    const containerClient = blobServiceClient.getContainerClient(containerName)
-    const content = "Hello world!";
-    const blobName = "newblob" + new Date().getTime();
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-    console.log(blockBlobClient)
-    console.log('=========')
-    return blockBlobClient.upload(content, content.length)
-
-  })
-  .then(uploadBlobResponse => {
-    console.log('*******')
-    console.log(`Upload block blob successfully`, uploadBlobResponse.requestId);
-    //for await (const blob of iter) {
-    //  console.log(`Blob ${i++}: ${blob.name}`)
-    //}
-    process.exit(0)
-  })
-  */
-  .then(async () => {
-    const ajv = new Ajv()
-    const validate = ajv.compile(schema)
-    if (validate(message)) {
-      console.log('SUCESS, MESSAGE VALID')
-      await sendToDynamics(schema, 'schema')
-      await sendToDynamics(message, 'message')
-      process.exit(0)
-    } else {
-      console.error('FAIL, INVALID MESSAGE')
-      process.exit(1)
-    }
-  })
-  .catch((ex) => {
-    console.error(ex.message)
-    process.exit(1)
-  })
