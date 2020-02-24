5ca483193ffa6e69a8def831badc575cf9aed190
rudenoise <rudenoise@gmail.com>
2020-02-24 15:18:33 +0000

    WE-3540: add basics of JSON schema

--- data_transfer/src/app.js
+++ data_transfer/src/app.js
@@ -1,7 +1,23 @@
 'use strict'
 
 const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob")
-const {promisify} = require('util')
+const Ajv = require("ajv")
+const schema = require("./hello.schema.json")
+const message = require("./hello.message.json")
+
+async function sendToDynamics(obj, label) {
+  const content = JSON.stringify(obj)
+
+  const blobServiceClient = await BlobServiceClient
+    .fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING)
+  const containerName = process.env.BLOB_CONTAINER_NAME
+  const containerClient = blobServiceClient.getContainerClient(containerName)
+  const blobName = label + new Date().getTime();
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const uploadResult = await blockBlobClient.upload(content, content.length)
+  console.log(`Upload block blob successfully`, uploadResult.requestId);
+
+}
 
 async function main() {
     console.log('Azure Blob storage v12 - JavaScript quickstart sample');
@@ -9,6 +25,7 @@ async function main() {
 }
 
 main()
+  /*
   .then(async () => {
     console.log('BLOB_CONTAINER_NAME:', process.env.BLOB_CONTAINER_NAME)
     console.log('AZURE_STORAGE_CONNECTION_STRING:', process.env.AZURE_STORAGE_CONNECTION_STRING)
@@ -35,6 +52,20 @@ main()
     //  console.log(`Blob ${i++}: ${blob.name}`)
     //}
     process.exit(0)
+  })
+  */
+  .then(async () => {
+    const ajv = new Ajv()
+    const validate = ajv.compile(schema)
+    if (validate(message)) {
+      console.log('SUCESS, MESSAGE VALID')
+      await sendToDynamics(schema, 'schema')
+      await sendToDynamics(message, 'message')
+      process.exit(0)
+    } else {
+      console.error('FAIL, INVALID MESSAGE')
+      process.exit(1)
+    }
   })
   .catch((ex) => {
     console.error(ex.message)
