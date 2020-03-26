aa9f9a742ff379bb5ea357338c6520a81d51176e
ben <ben@surgison.net>
2020-03-26 21:19:00 +0000

    Implement post properly

--- poc_flow_map_front_end/src/server/modules/tasklist/tasklist-init.route.js
+++
@@ -1,9 +0,0 @@
-const { setQueryData } = require('hapi-govuk-journey-map')
-
-module.exports = {
-  method: 'GET',
-  handler: async function (request, h) {
-    await setQueryData(request, { task: undefined, tasksComplete: undefined })
-    return h.continue
-  }
-}
--- poc_flow_map_front_end/src/server/modules/tasklist/tasklist.route.js
+++ poc_flow_map_front_end/src/server/modules/tasklist/tasklist.route.js
@@ -1,7 +1,6 @@
-const { getQueryData, setQueryData } = require('hapi-govuk-journey-map')
+const { setQueryData } = require('hapi-govuk-journey-map')
 const Joi = require('@hapi/joi')
-const Hoek = require('@hapi/hoek')
-const { formatErrors } = require('../../utils/validation')
+const failAction = require('../../utils/validation')
 const Application = require('../../dao/application')
 const Tasks = require('../../dao/tasks')
 
@@ -41,37 +40,46 @@ async function buildTaskList (request) {
     .filter(({ tasks }) => tasks.length)
 }
 
+async function tasksComplete (request) {
+  const { tasks: permitTasks } = await permitReference(request)
+  const taskStatus = await Tasks.get(request)
+  const remainingTasks = tasklistReference
+    .map(({ tasks, submit }) => submit ? [] : tasks.filter(({ id, required }) => !taskStatus[id] && (required || permitTasks.includes(id))))
+    .flat()
+  return !remainingTasks.length
+}
+
 async function getErrors (request) {
-  const { tasksComplete = 'yes' } = await getQueryData(request)
   const schema = Joi.object({ tasksComplete: Joi.string().valid('yes') })
-  const { error } = schema.validate({ tasksComplete })
+  const { error } = schema.validate({ tasksComplete: await tasksComplete(request) ? 'yes' : 'no' })
   return error
+}
+
+async function getViewData (request) {
+  const tasklist = await buildTaskList(request)
+  return {
+    pageHeading: pageHeading,
+    value: { tasklist }
+  }
 }
 
 module.exports = [{
   method: 'GET',
   handler: async function (request, h) {
-    const tasklist = await buildTaskList(request)
-    const viewData = {
-      pageHeading: pageHeading,
-      value: { tasklist }
-    }
-
-    const errors = await getErrors(request)
-    if (errors) {
-      // Merge the viewData with the formatted error messages
-      Hoek.merge(viewData, await formatErrors(errors, { tasksComplete: { 'any.only': 'You must complete all the tasks before continuing' } }))
-    } else {
-      const { task } = await getQueryData(request)
-      if (task === 'check-your-answers') {
-        await setQueryData(request, { task: undefined, tasksComplete: undefined })
-        return h.continue // Will redirect to next route
-      }
-    }
-
-    return h.view(view, viewData)
+    return h.view(view, await getViewData(request))
   },
   options: {
     tags: ['hide-back-link']
+  }
+}, {
+  method: 'POST',
+  handler: async function (request, h) {
+    const errors = await getErrors(request)
+    if (errors) {
+      await setQueryData(request, { task: undefined })
+      const displayErrors = failAction(view, await getViewData(request), { tasksComplete: { 'any.only': 'You must complete all the tasks before continuing' } })
+      return displayErrors(request, h, errors)
+    }
+    return h.continue
   }
 }]
