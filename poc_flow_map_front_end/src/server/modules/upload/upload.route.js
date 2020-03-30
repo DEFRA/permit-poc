387f71502e6d2eb0f6cbe92d0e7fe35bd5b4e720
Stuart Adair <stu@silverka.co.uk>
2020-03-30 10:56:51 +0100

    Use filetype passed through from yml

--- poc_flow_map_front_end/src/server/modules/upload/upload.route.js
+++ poc_flow_map_front_end/src/server/modules/upload/upload.route.js
@@ -1,6 +1,3 @@
-// TODO: Pull the filetype for the files list from the map.yaml
-// TODO: Populate uploadedFiles on page load (by searching files list for filetype?)
-
 const Application = require('../../dao/application')
 const Joi = require('@hapi/joi')
 const failWith = require('../../utils/validation')
@@ -8,7 +5,7 @@ const view = 'upload/upload.view.njk'
 const Hoek = require('@hapi/hoek')
 const Bourne = require('@hapi/bourne')
 
-const pageHeading = 'Upload a file'
+const { getCurrent } = require('hapi-govuk-journey-map')
 
 // As of v16 Joi no longer parses JSON arrays
 // So we extend Joi to do this ourselves
@@ -32,27 +29,35 @@ const coerceArrayJoi = Joi.extend((joi) => {
 module.exports = [{
   method: 'GET',
   handler: async function (request, h) {
-    const { files = [] } = await Application.get(request)
-    const uploadedFiles = files
-    return h.view(view, { pageHeading, uploadedFiles, formOptions: 'enctype="multipart/form-data"' })
+    console.log(await getFormattedFiles(request))
+    return h.view(view, {
+      pageHeading: await getPageHeading(request),
+      uploadedFiles: await getFormattedFiles(request),
+      formOptions: 'enctype="multipart/form-data"'
+    })
   }
 }, {
   method: 'POST',
   handler: async function (request, h) {
     // Parse the JSON file list we've been passed
     const fileList = (request.payload.files)
+    const filetype = await getFiletype(request)
 
     // Add the filetype to each one
-    fileList.forEach((file) => { file.filetype = 'POC demo file' })
+    fileList.forEach(file => { file.filetype = filetype })
 
     // Get the current list of files (defaulting to an empty array)
     const { files = [] } = await Application.get(request)
 
+    // Filter out files with the current filetype so we can replace them with
+    // the current set of files of that filetype
+    const filteredFiles = files.filter(file => file.filetype !== filetype)
+
     // Merge the uploaded files
-    Hoek.merge(files, fileList)
+    Hoek.merge(filteredFiles, fileList)
 
     // Save the list back
-    await Application.update(request, { files })
+    await Application.update(request, { files: filteredFiles })
 
     return h.continue
   },
@@ -64,11 +69,55 @@ module.exports = [{
       payload: Joi.object({
         files: coerceArrayJoi.array().min(1)
       }).options({ allowUnknown: true, convert: true }),
-      failAction: failWith(view, { pageHeading }, {
-        files: {
-          'array.min': 'You must upload a file'
-        }
-      })
+      failAction: async (request) => {
+        const route = await getCurrent(request)
+        const pageHeading = route.parent.options.heading
+        return failWith(view, { pageHeading }, {
+          files: {
+            'array.min': 'You must upload a file'
+          }
+        })
+      }
     }
   }
 }]
+
+const getPageHeading = async (request) => {
+  const route = await getCurrent(request)
+  return route.parent.options.title
+}
+
+const getFiletype = async (request) => {
+  const route = await getCurrent(request)
+  return route.parent.options.filetype
+}
+
+const getUploadedFiles = async (request) => {
+  // Get the current list of files (defaulting to an empty array)
+  const { files = [] } = await Application.get(request)
+  const filetype = await getFiletype(request)
+
+  // Filter and return the list of files to just those matching the desired filetype
+  return files.filter(file => file.filetype === filetype)
+}
+
+// MOJ multi-file upload component expects file list to be in a specific format
+const getFormattedFiles = async (request) => {
+  const fileList = await getUploadedFiles(request)
+
+  return fileList.map(item => ({
+    fileName: item.id,
+    originalFileName: item.filename,
+    message: {
+      html: getSuccessHtml({ messageHtml: item.filename })
+    },
+    deleteButton: {
+      text: 'Delete'
+    }
+  }))
+}
+
+// Lifted directly from moj-frontend/src/moj/components/multi-file-upload/multi-file-upload.js
+const getSuccessHtml = function (success) {
+  return '<span class="moj-multi-file-upload__success"> <svg class="moj-banner__icon" fill="currentColor" role="presentation" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" height="25" width="25"><path d="M25,6.2L8.7,23.2L0,14.1l4-4.2l4.7,4.9L21,2L25,6.2z"/></svg> ' + success.messageHtml + '</span>'
+}
