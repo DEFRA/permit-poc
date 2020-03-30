9125a3660c318318a2439ae08525366c5e17ffea
ben <ben@surgison.net>
2020-03-30 10:34:15 +0100

    Check your answers

--- poc_flow_map_front_end/src/server/modules/tasklist/tasklist.route.js
+++ poc_flow_map_front_end/src/server/modules/tasklist/tasklist.route.js
@@ -1,53 +1,10 @@
 const { setQueryData } = require('hapi-govuk-journey-map')
 const Joi = require('@hapi/joi')
 const failAction = require('../../utils/validation')
-const Application = require('../../dao/application')
-const Tasks = require('../../dao/tasks')
-
-const { load } = require('../../utils/reference')
 const view = 'tasklist/tasklist.view.njk'
 const pageHeading = 'Apply for a standard rules environmental permit'
-const tasklistReference = load('tasklist')
 
-async function permitReference (request) {
-  const { permitType, permitCategory, standardRule } = await Application.get(request)
-  return require(`../../reference/${permitType}/${permitCategory}/${standardRule}`)
-}
-
-function getState (state) {
-  switch (state) {
-    case true: return { id: 'completed', label: 'Completed' }
-    case false: return { id: 'incomplete', label: 'Incomplete' }
-    default: return { id: 'not-started', label: 'Not started' }
-  }
-}
-
-async function buildTaskList (request) {
-  const { tasks: permitTasks } = await permitReference(request)
-  const taskStatus = await Tasks.get(request)
-  return tasklistReference
-    .map(({ heading, tasks, submit }) => {
-      return {
-        heading,
-        tasks: tasks
-          .filter(({ id, required }) => required || permitTasks.includes(id))
-          .map((task) => {
-            return { ...task, submit, state: getState(taskStatus[task.id]), link: `/task/${task.id}` }
-          })
-      }
-    })
-    // Only include sections containing tasks
-    .filter(({ tasks }) => tasks.length)
-}
-
-async function tasksComplete (request) {
-  const { tasks: permitTasks } = await permitReference(request)
-  const taskStatus = await Tasks.get(request)
-  const remainingTasks = tasklistReference
-    .map(({ tasks, submit }) => submit ? [] : tasks.filter(({ id, required }) => !taskStatus[id] && (required || permitTasks.includes(id))))
-    .flat()
-  return !remainingTasks.length
-}
+const { buildTaskList, tasksComplete } = require('../../utils/taskReference')
 
 async function getErrors (request) {
   const schema = Joi.object({ tasksComplete: Joi.string().valid('yes') })
---
+++ poc_flow_map_front_end/src/server/utils/taskReference.js
@@ -0,0 +1,52 @@
+
+const Application = require('../dao/application')
+const Tasks = require('../dao/tasks')
+
+const { load } = require('./reference')
+const tasklistReference = load('tasklist')
+
+function getState (state) {
+  switch (state) {
+    case true: return { id: 'completed', label: 'Completed' }
+    case false: return { id: 'incomplete', label: 'Incomplete' }
+    default: return { id: 'not-started', label: 'Not started' }
+  }
+}
+
+async function permitReference (request) {
+  const { permitType, permitCategory, standardRule } = await Application.get(request)
+  return require(`../reference/${permitType}/${permitCategory}/${standardRule}`)
+}
+
+async function buildTaskList (request) {
+  const { tasks: permitTasks } = await permitReference(request)
+  const taskStatus = await Tasks.get(request)
+  return tasklistReference
+    .map(({ heading, tasks, submit }) => {
+      return {
+        heading,
+        submit,
+        tasks: tasks
+          .filter(({ id, required }) => required || permitTasks.includes(id))
+          .map((task) => {
+            return { ...task, submit, state: getState(taskStatus[task.id]), link: `/task/${task.id}` }
+          })
+      }
+    })
+    // Only include sections containing tasks
+    .filter(({ tasks }) => tasks.length)
+}
+
+async function tasksComplete (request) {
+  const { tasks: permitTasks } = await permitReference(request)
+  const taskStatus = await Tasks.get(request)
+  const remainingTasks = tasklistReference
+    .map(({ tasks, submit }) => submit ? [] : tasks.filter(({ id, required }) => !taskStatus[id] && (required || permitTasks.includes(id))))
+    .flat()
+  return !remainingTasks.length
+}
+
+module.exports = {
+  tasksComplete,
+  buildTaskList
+}
