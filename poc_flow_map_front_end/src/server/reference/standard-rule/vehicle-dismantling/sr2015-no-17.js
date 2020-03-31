a9ff266b0b74ff3883ecb877a6037a017264e088
Stuart Adair <stu@silverka.co.uk>
2020-03-31 15:14:21 +0100

    Remove tasks not yet developed ahead of demo

--- poc_flow_map_front_end/src/server/reference/standard-rule/vehicle-dismantling/sr2015-no-17.js
+++ poc_flow_map_front_end/src/server/reference/standard-rule/vehicle-dismantling/sr2015-no-17.js
@@ -23,9 +23,9 @@ const costAndActivities = [
 ]
 
 const tasks = [
-  'contact-details',
-  'site-name',
-  'site-plan'
+  // 'contact-details',
+  // 'site-name',
+  // 'site-plan'
 ]
 
 module.exports = {
